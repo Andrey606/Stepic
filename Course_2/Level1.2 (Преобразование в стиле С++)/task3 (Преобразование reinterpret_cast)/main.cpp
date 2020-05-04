469c4b3b44c5af7fd9ad6c985e778fa19d2fbb2c
andreykuluev96 <andreykuluev96@omo.systems>
2020-05-04 23:11:09 +0300

    1

--- Course_2/Level1.2 (Преобразование в стиле С++)/task3 (Преобразование reinterpret_cast)/main.cpp
+++ Course_2/Level1.2 (Преобразование в стиле С++)/task3 (Преобразование reinterpret_cast)/main.cpp
@@ -7,11 +7,15 @@ using namespace std;
 
 int main(int argc, const char * argv[]) 
 {
+    // ниже так же работает и с сылками
     double *double_array = new double[100];
     cout << typeid(double_array).name() << endl;
 
     char *char_array = reinterpret_cast<char *>(double_array);
     cout << typeid(char_array).name() << endl;
-    
+
+    // можно преобразовать число в указатель и наоборот
+    size_t ms = reinterpret_cast<size_t>(double_array);
+
     return 0;
 }
