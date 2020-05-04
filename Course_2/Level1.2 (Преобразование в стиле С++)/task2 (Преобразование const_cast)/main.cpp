3a8d4b95c4f7ef19c312274425eabe86b83f9570
andreykuluev96 <andreykuluev96@omo.systems>
2020-05-04 18:17:31 +0300

    1

--- Course_2/Level1.2 (Преобразование в стиле С++)/task2 (Преобразование const_cast)/main.cpp
+++ Course_2/Level1.2 (Преобразование в стиле С++)/task2 (Преобразование const_cast)/main.cpp
@@ -12,11 +12,66 @@ void foo(double const & d)
 
 int main(int argc, const char * argv[]) 
 {
-    double a = 1;
+    // 0
+    int Num = 10;
+    int Num2 = 10;
 
-    cout << a << endl;
-    foo(a);
-    cout << a << endl;
+    // значение константоное
+    /*const int *numPtr = &Num;
+    int const *numPtr2 = &Num;
+    *numPtr = 11; // ошибка
+    *numPtr2 = 12; // ошибка */
+
+    // указатель константный
+    /*
+    int *const numPtr3 = &Num;
+    *numPtr3 = 11; // ок
+    numPtr3 = &Num2;// ошибка */
+
+    
+    // 1
+    /*double num = 1;
+
+    cout << num << endl;
+    foo(num);
+    cout << num << endl;*/
+
+    // 2
+    int a = 27;
+    int const b = 412;
+    int * pa = &a;
+
+    int const c = const_cast<int const &>(a);
+    int const c2 = a;
+    cout << typeid(c).name() << endl;
+    cout << typeid(c2).name() << endl;
+
+    int d = const_cast<int &>(b);
+    int d2 = b;
+    cout << typeid(d).name() << endl;
+    cout << typeid(d2).name() << endl;
+
+    int const * p1 = const_cast<int const *>(pa);
+    int const * p11 = pa;
+    cout << typeid(p1).name() << endl;
+    cout << typeid(p11).name() << endl;
+
+    int * const * p2 = const_cast<int * const *>(&pa);
+    //int * const * p22 = pa; // ошибка
+    cout << typeid(p2).name() << endl;
+
+    int const ** p3 = const_cast<int const **>(&pa);
+    //int const ** p33 = pa; // ошибка
+    cout << typeid(p3).name() << endl;
+
+    int const * const * p4 = const_cast<int const * const *>(&pa);
+    int const * const * p4 = &pa; // ошибка
+
+
+
+
+
+
 
     return 0;
 }
