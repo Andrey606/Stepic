21a1b1ef0be2587f665ef50663d395f2c0c15ec8
andreykuluev96 <andreykuluev96@omo.systems>
2020-05-04 19:05:58 +0300

    1

--- Course_2/Level1.2 (Преобразование в стиле С++)/task3 (Преобразование reinterpret_cast)/main.cpp
+++ Course_2/Level1.2 (Преобразование в стиле С++)/task3 (Преобразование reinterpret_cast)/main.cpp
@@ -7,7 +7,11 @@ using namespace std;
 
 int main(int argc, const char * argv[]) 
 {
-   
+    double *double_array = new double[100];
+    cout << typeid(double_array).name() << endl;
 
+    char *char_array = reinterpret_cast<char *>(double_array);
+    cout << typeid(char_array).name() << endl;
+    
     return 0;
 }
