e4ec7f915896676fd777b38643b048771d4f034c
Andrey606 <andreykuluev96@gmail.com>
2020-05-09 01:46:55 +0300

    1

--- Course_2/Level1.4 (Информация о типах времени выполнения)/task2 () /main.cpp
+++ Course_2/Level1.4 (Информация о типах времени выполнения)/task2 () /main.cpp
@@ -5,24 +5,9 @@
 
 using namespace std;
 
-struct Unit
-{
-    virtual ~Unit() {}
-};
-
-struct Elf: Unit {};
-
 int main(int argc, const char * argv[]) 
 {
-    Elf e;
-    Unit &ur = e;
-    Unit *up = &e;
- 
-    cout << typeid(ur).name() << endl; // Elf
-    cout << typeid(*up).name() << endl; // Elf
-    cout << typeid(up).name() << endl; // Unit
-    cout << typeid(Elf).name() << endl; // Elf
-    cout << (typeid(ur) == typeid(Elf)) << endl; // 1
+    
 
     return 0;
 }
