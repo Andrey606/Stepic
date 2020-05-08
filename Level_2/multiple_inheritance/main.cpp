cd3d96df591d15e81b24a7ed10d419cc7a1e7049
Andrey606 <andreykuluev96@gmail.com>
2020-05-09 01:26:37 +0300

    12

---
+++ Level_2/multiple_inheritance/main.cpp
@@ -0,0 +1,36 @@
+/*
+* Умные указатели. Smart pointers. Изучение С++ для начинающих. Урок #130
+*/
+
+#include <iostream>
+
+using namespace std;
+
+class Base{
+    virtual void method() {cout << "from Base" << endl;}
+
+public:
+    virtual ~Base(){method();}
+    void baseMethod() {method();}
+};
+
+class A: public Base{
+    void method() {cout << "from A" << endl;}
+
+public:
+    ~A(){method();}
+};
+
+
+int main() 
+{
+    Base* base = new A;
+
+    cout << "1" << endl;
+    base->baseMethod();
+
+    cout << "2" << endl;
+    delete base;
+   
+    return 0;
+}
